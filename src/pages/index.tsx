78d69f360d81fdcb9f62bfaaf2218c0b9691513e
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-01-05 19:49:57 -0800

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import dynamic from "next/dynamic"
-
-const DynamicMap = dynamic(() => import("../../components/Map"), { ssr: false });
+import dynamic from 'next/dynamic';
+const DynamicMap = dynamic(() => import('../../components/Map'), { ssr: false });
 
 export default function Home() {
   return (
     <>
+      <header className="bg-blue-500 text-white p-4 text-center flex flex-col items-center">
+        <img src="/images/canada-line-logo.png" alt="Logo" className="h-20" />
+        <p className="text-md font-bold mt-4">Explore the lower mainland on the Canada Line</p>
+      </header>
       <DynamicMap />
     </>
-  )
+  );
 }
