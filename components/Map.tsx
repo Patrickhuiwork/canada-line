1fd7d24f914937c885fa704d0fc33c605845a754
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-01-05 17:22:41 -0800

    data files

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,34 +1,16 @@
-import {MapContainer, TileLayer, Marker, Popup, Polyline, Circle, CircleMarker} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup, Polyline} from 'react-leaflet'
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'
+import canadaLineCoordinates from '../data/canadaLine';
+import canadaLineStations from '../data/stationmarkers';
 
 
-const icon = L.icon({ iconUrl: '/images/marker-icon.png', iconSize: [35, 50] }) // Adjust the iconSize to decrease the scale of the marker
+const icon = L.icon({ iconUrl: '/images/station-icon.png', iconSize: [35, 50] })
 
-const polyline:[number, number][] = [
-    [49.25299, -123.00600],
-    [49.25980, -123.01400],
-    [49.25299, -123.04400],
-]
-
-const multipolyline:[number, number][][] = [
-    [
-        [49.25299, -123.00800],
-        [49.25980, -123.03300],
-        [49.25299, -123.07400],
-
-    ],
-    [
-        [49.25299, -123.00800],
-        [49.25555, -123.03300],
-        [49.25777, -123.07400],
-
-    ]
-]
+const polyline: [number, number][] = canadaLineCoordinates;
 
 const redOptions = { color: 'red' }
-const orangeOptions = { color: 'orange' }
-const fillBlueOptions = { color: 'blue' }
+const blueOptions = { color: 'blue' }
 
 const center:[number, number] = [49.25299, -123.00600];
 
@@ -39,25 +21,16 @@ function Map() {
         center={[49.24966, -123.00934]} zoom={14} scrollWheelZoom={true}
     >
         <TileLayer
-            attribution='&copy; <a href="">By Patrick Hui</a> contributors'
+            attribution='&copy; <a href="https://github.com/Patrickhuiwork/canada-line.git">By Patrick Hui</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[49.25299, -123.00600]} icon={icon}>
-                <Popup>
-                    BCITjhgjhgj
-                </Popup>
-        </Marker>
+            {canadaLineStations.map((station, index) => (
+                    <Marker key={`marker-${index}`} position={station.position} icon={icon}>
+                    <Popup>{station.name}</Popup>
+                    </Marker>
+                ))}
         <Polyline pathOptions={redOptions} positions={polyline} />
-        <Polyline pathOptions={orangeOptions} positions={multipolyline} />
-        <Circle center={center} pathOptions={fillBlueOptions} radius={200}/>
-
-        <CircleMarker center={[49.25299, -123.01650]} pathOptions={redOptions} radius={20}>
-            <Popup>
-                circle
-            </Popup>
-        </CircleMarker>
     </MapContainer>
-
   )
 }
 
---
+++ data/stationmarkers.ts
@@ -0,0 +1,20 @@
+const canadaLineStations: { position: [number, number]; name: string }[] = [
+    { position: [49.2856, -123.1115], name: 'Waterfront' },
+    { position: [49.2825, -123.1186], name: 'Vancouver City Centre' },
+    { position: [49.2746, -123.1219], name: 'Yaletown-Roundhouse Station' },
+    { position: [49.2665, -123.1156], name: 'Olympic Village Station' },
+    { position: [49.2628, -123.1145], name: 'Broadway-City Hall Station' },
+    { position: [49.2492, -123.1159], name: 'King Edward Station' },
+    { position: [49.2332, -123.1166], name: 'Oakridge-41st Station' },
+    { position: [49.2263, -123.1161], name: 'Langara-49th Station' },
+    { position: [49.2097, -123.1170], name: 'Marine Drive Station' },
+    { position: [49.1955, -123.1261], name: 'Bridgeport Station' },
+    { position: [49.1967, -123.1463], name: 'Templeton Station' },
+    { position: [49.1930, -123.1579], name: 'Sea Island Centre Station' },
+    { position: [49.1942, -123.1783], name: 'YVR-Airport Station' },
+    { position: [49.1840, -123.1364], name: 'Aberdeen Station' },
+    { position: [49.1748, -123.1364], name: 'Lansdowne Station' },
+    { position: [49.1681, -123.1372], name: 'Richmond-Brighouse Station' },
+  ];
+  
+  export default canadaLineStations;
